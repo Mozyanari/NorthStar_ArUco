e8b5fd7fae64f357be83c6ed1c2f9e19da6eb66d
LabPC <mozyanari@outloo.jp>
2019-05-09 11:44:54 +0900

    test

---
+++ Assets/Scripts/opencv_test.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCvSharp;
+using OpenCvSharp.Aruco;
+
+public class opencv_test : MonoBehaviour {
+    private int source = 0; //カメラのインデックス
+
+    private VideoCapture cam;
+    public Mat cam_frame;
+
+    private Texture2D cam_Texture;
+
+    /* ArUcoの設定 */
+    //ARマーカ辞書の設定
+    private const PredefinedDictionaryName dictName = PredefinedDictionaryName.Dict4X4_50;
+    private Dictionary ar_dict;
+    //検出アルゴリズムの設定
+    private DetectorParameters detect_param;
+
+    // Use this for initialization
+    void Start () {
+        cam = new VideoCapture(source);
+        cam.Set(CaptureProperty.ConvertRgb, 3);
+        cam_frame = new Mat();
+
+        ar_dict = CvAruco.GetPredefinedDictionary(dictName);
+        detect_param = DetectorParameters.Create();
+
+        cam_Texture = new Texture2D(
+            cam.FrameWidth,    //カメラのwidth
+            cam.FrameHeight,    //カメラのheight
+            TextureFormat.RGB24, //フォーマットを指定
+            false               //ミニマップ設定
+        );
+        this.GetComponent<Renderer>().material.mainTexture = cam_Texture;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Point2f[][] maker_corners;  //ARマーカのカドの座標
+        int[] maker_ids;            //検出されたARマーカのID
+        Point2f[][] reject_points;
+
+        cam.Read(cam_frame);//フレームの更新
+
+        //ARマーカの検出
+        CvAruco.DetectMarkers(cam_frame, ar_dict, out maker_corners, out maker_ids, detect_param, out reject_points);
+
+        //キャリブレーションデータを設定
+        double[,] array3_3 = new double[3, 3] { { 548.8, 0.0, 332.1 }, { 0.0, 549.8, 236.2 }, { 0.0, 0.0, 1.0 } };
+        Mat cameraMatrix = new Mat(3,3, MatType.CV_32FC1, array3_3);
+
+        double[,] array1_5 = new double[1, 5] { { -0.0886, 0.4802, -0.0026, 0.0019 , -0.8238 } };
+        Mat distortionCoefficients = new Mat(1, 5, MatType.CV_32FC1, array1_5);
+
+        
+        
+        
+        
+        var rvec = new Mat();
+        var tvec = new Mat();
+        
+
+        
+
+        //VectorOfVec3f vector3_rvec = new VectorOfVec3f();
+        //IEnumerable<VectorOfVec3f> vector3_rvec = new IEnumerable<VectorOfVec3f>;
+        
+
+
+
+        if (maker_ids.Length != 0)
+        {
+            //検出されたマーカ情報の描画
+            CvAruco.DrawDetectedMarkers(cam_frame, maker_corners, maker_ids, new Scalar(0, 255, 0));
+            //Debug.Log("Find");
+
+            /*
+            Mat rvec = new Mat(3, maker_ids.Length, MatType.CV_32FC1);
+            Mat tvec = new Mat(3, maker_ids.Length, MatType.CV_32FC1);
+
+            
+
+            Debug.Log(tvec.Rows);
+            Debug.Log(tvec.Cols);
+            Debug.Log(tvec.Size());
+            Debug.Log(tvec.Dims());
+            */
+
+            CvAruco.EstimatePoseSingleMarkers(maker_corners, (float)1.8, cameraMatrix, distortionCoefficients, rvec, tvec);
+            
+
+            //マーカまでの距離
+
+            /*
+            Debug.Log(tvec.Rows);
+            Debug.Log(tvec.Cols);
+            Debug.Log(tvec.Size());
+            Debug.Log(tvec.Dims());
+            */
+            
+            //Debug.Log(tvec[new OpenCvSharp.Rect(0, 1,0,1)]);
+            
+            
+            /*
+            Debug.Log(rvec.Rows);
+            Debug.Log(rvec.Cols);
+            Debug.Log(rvec.Size());
+            Debug.Log(rvec.Dims());
+            */
+
+            /*
+            //Debug.Log(cameraMatrix.Size());
+            //1
+            Debug.Log(distortionCoefficients.Rows);
+            //5
+            Debug.Log(distortionCoefficients.Cols);
+            Debug.Log(distortionCoefficients.Size());
+            */
+
+            //Debug.Log(tvec.)
+            //24
+            //Debug.Log(tvec.Step());
+            //3
+            //Debug.Log(tvec.Channels());
+
+
+
+            
+            Debug.Log(tvec.At<double>(0, 0));
+            Debug.Log(tvec.At<double>(0, 1));
+            Debug.Log(tvec.At<double>(0, 2));
+            
+            /*
+            Debug.Log(rvec.GetArray(0, 1));
+            Debug.Log(rvec.GetArray(0, 2));
+            */
+            
+        }
+
+        cam_Texture.LoadImage(cam_frame.ImEncode());
+    }
+}
---
+++ Assets/Scripts/web_camera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCvSharp;
+
+public class web_camera : MonoBehaviour
+{
+    [SerializeField] private int m_WebCamIndex = 1;
+    private Texture2D texture2D;
+    private VideoCapture videoCapture;
+    private Mat mat;
+    private Mat grey;
+
+    // Use this for initialization
+    void Start()
+    {
+        videoCapture = new VideoCapture(m_WebCamIndex);
+        texture2D = new Texture2D(videoCapture.FrameWidth, videoCapture.FrameHeight, TextureFormat.RGB24, false);
+        GetComponent<Renderer>().material.mainTexture = texture2D;
+        mat = new Mat();
+        grey = new Mat();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        videoCapture.Read(mat);
+        Cv2.CvtColor(mat, grey, ColorConversionCodes.BGR2GRAY);
+        texture2D.LoadImage(grey.ImEncode());
+    }
+    /*
+    private void OnDestroy()
+    {
+        videoCapture.Dispose();
+        videoCapture = null;
+    }
+    */
+}
